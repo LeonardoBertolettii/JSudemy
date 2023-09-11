a32c021c02c5e4a0371bf3a7f2d316151e46b129
Leonardo Bertoletti <Leonardo.Bertoletti@edu.pucrs.br>
2023-09-11 19:10:52 -0300

    Aula basica de objetos

---
+++ Objetos/app.js
@@ -0,0 +1,24 @@
+
+const pessoa1 = {
+    nome: `leonardo`,
+    sobrenome: `bertoletti`,
+    idade: 23,
+    
+    fala() {console.log(`${this.nome} diz oi`)}
+}
+
+// outra maneira mais parecido com o java pra criar varios 
+function CriaPessoa (nome,sobrenome, idade) {
+    return{
+        //nome: nome,
+        //sobrenome: sobrenome,
+        //idade: idade,
+        nome, sobrenome, idade, //ele entendo quando for valores de nomes iguais igual esta comentado em cima 
+
+    
+    }
+}
+let num = 1
+
+const p1 = CriaPessoa(`Leonardo`,`Bertoletti`,23)
+console.log(p1.Fala())
